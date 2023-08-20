dcd0dc62fd250ad0dcf5c0b13d228f536723ff88
dushyantxo <141238622+dushyantxo@users.noreply.github.com>
2023-08-20 19:51:46 +0530

    Add files via upload

---
+++ ncert exempler/codes/main.py
@@ -0,0 +1,11 @@
+# Total number of cards in the deck
+total_cards = 52
+
+# Number of ace of hearts in the deck (1 ace of hearts)
+ace_of_hearts = 1
+
+# Calculate the number of outcomes favorable to E
+favorable_outcomes = total_cards - ace_of_hearts
+
+print("Number of favorable outcomes to event E:", favorable_outcomes)
+
